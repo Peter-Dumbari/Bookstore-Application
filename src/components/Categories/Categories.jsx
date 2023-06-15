1672fef2c085c0e97c6d41f1af7d6177649cf809
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-06-15 23:29:48 +0100

    linter error resolved

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 // import './Categories.css';
 
 function Categories() {
--- src/components/CreateBookForm/Createbookform.jsx
+++ src/components/CreateBookForm/Createbookform.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 // import './Createbookform.css';
 
 function Createbookform() {
